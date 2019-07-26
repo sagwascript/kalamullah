9f11882082861c6aff23c20b1dfd8303c5819087
Muhammad Riza <muh.riza.burhani12@gmail.com>
2019-07-26 08:07:44 +0700

    move translation directory

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react"
+import ReactDOM from "react-dom"
+import App from './App'
+
+ReactDOM.render(<App />, document.getElementById("root"))
+// render and attach the element into the 'root' 😄 
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist/'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react']
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: '/\.txt$/',
+                use: 'raw-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    }
+}
